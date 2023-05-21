0af759755e3fe86afc69144abe61214bb310b5ff
A.Farel.E <99515298+FarelE@users.noreply.github.com>
2023-05-21 19:35:07 +0700

    Add files via upload

--- kyaaa.js
+++ kyaaa.js
@@ -950,6 +950,29 @@ sock.sendMessage(from, buttonMessage)
             sock.sendPresenceUpdate('unavailable', m.chat)
         }
 
+async function tebakanime() {
+    let limit = Math.floor(Math.random() * (500 - 1 + 1) + 1)
+    let html = await (await axios.get('https://myanimelist.net/character.php?limit=' + limit)).data
+    $ = cheerio.load(html)
+    let collect = { r: [], anime: [] }
+    $('table > tbody > tr > td').each(function (i, e) {
+        let txt = $(e).find('a').text().trim()
+        let url
+        url = $(e).find('img').attr('data-srcset') ? $(e).find('img').attr('data-srcset') : $(e).find('a').attr('href')
+        let low = url ? url.replace('https://', '').split('/')[1] : ''
+        collect[low]?.push({ text: txt.replace(',', ''), url })
+    })
+    let rand = Math.floor(Math.random() * collect.r.length)
+    let r = collect.r[rand]
+    let anime = collect.anime[rand]
+    return { 
+     status: true, 
+     name: r.text, 
+     anime: anime.text != null ? anime.text : '', 
+     img: r.url.split(',')[1].trim().split(' ')[0]
+    }
+}
+
 // By FarelAE
 function igstalk1(nama) {
 const url = `https://www.inststalk.com/search?user=${nama}`
@@ -3039,7 +3062,7 @@ await sleep(1000)
         reply(anu)
         }
         break
-case 'broadcast': case 'bcgc': {
+case 'broadcast': case 'bc': {
 if (!isOwner) return untukowner();
 let anu = Object.keys(global.db.pengguna)
 

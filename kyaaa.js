2221263ba0a68f855ef2670ff7d46b71ac03da55
A.Farel.E <99515298+FarelE@users.noreply.github.com>
2023-05-21 19:47:11 +0700

    Add files via upload

--- kyaaa.js
+++ kyaaa.js
@@ -953,12 +953,12 @@ sock.sendMessage(from, buttonMessage)
 async function tebakanime() {
     let limit = Math.floor(Math.random() * (500 - 1 + 1) + 1)
     let html = await (await axios.get('https://myanimelist.net/character.php?limit=' + limit)).data
-    $ = cheerio.load(html)
+    let $ = cheerio.load(html)
     let collect = { r: [], anime: [] }
     $('table > tbody > tr > td').each(function (i, e) {
         let txt = $(e).find('a').text().trim()
         let url
-        url = $(e).find('img').attr('data-srcset') ? $(e).find('img').attr('data-srcset') : $(e).find('a').attr('href')
+        let url = $(e).find('img').attr('data-srcset') ? $(e).find('img').attr('data-srcset') : $(e).find('a').attr('href')
         let low = url ? url.replace('https://', '').split('/')[1] : ''
         collect[low]?.push({ text: txt.replace(',', ''), url })
     })
